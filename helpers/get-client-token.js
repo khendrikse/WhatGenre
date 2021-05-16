39030add42ca037fef277bdda10ced262c4f80e5
khendrikse <30577427+khendrikse@users.noreply.github.com>
2021-05-16 14:31:55 +0200

    change baseurl for getclienttoken

--- helpers/get-client-token.js
+++ helpers/get-client-token.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 const getClientToken = clientToken =>
-  axios.get('http://localhost:3000/api/getClientToken').then(({ data }) => {
+  axios.get(`${baseUrl}/api/getClientToken'`).then(({ data }) => {
     // eslint-disable-next-line no-param-reassign
     clientToken.current = data.clientToken;
   });
