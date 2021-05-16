f67ed3bbc9f2c76de331cf3197a9fa1af046545b
khendrikse <k.hendrikse@youngcapital.nl>
2021-05-16 14:39:25 +0200

    remove parantheses

--- helpers/get-client-token.js
+++ helpers/get-client-token.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 const getClientToken = clientToken =>
-  axios.get(`${baseUrl}/api/getClientToken'`).then(({ data }) => {
+  axios.get(`${baseUrl}/api/getClientToken`).then(({ data }) => {
     // eslint-disable-next-line no-param-reassign
     clientToken.current = data.clientToken;
   });
