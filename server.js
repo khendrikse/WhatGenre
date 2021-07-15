7515bc028186cd316b84f3d859087d1c4cb3395f
khendrikse <30577427+khendrikse@users.noreply.github.com>
2021-07-15 17:07:10 +0200

    check port

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const port = process.env.PORT;
+const port = dev ? '5000' : process.env.PORT;
 const handle = app.getRequestHandler();
 
 app
   .prepare()
   .then(() => {
     const server = express();
-    console.log({ port: process.env.PORT });
     server.use((req, res, next) => {
       const hostname = process.env.HOSTNAME;
+      console.log({ port, redirect: `https://${hostname}${req.url}` });
       if (
         req.headers['x-forwarded-proto'] === 'http' ||
         req.hostname === hostname
@@ -32,7 +32,7 @@ app
 
     server.listen(port, error => {
       if (error) throw error;
-      console.error(`Listening on port ${process.env.PORT}`);
+      console.error(`Listening on port ${port}`);
     });
   })
   .catch(error => {
