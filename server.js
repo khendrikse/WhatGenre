87831c88abf692d7de137468f41223cad1f4002f
khendrikse <30577427+khendrikse@users.noreply.github.com>
2021-07-15 16:52:22 +0200

    redirect to https

--- server.js
+++ server.js
@@ -1,26 +1,41 @@
-const { createServer } = require('http');
-const { parse } = require('url');
+const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
-const port = process.env.PORT;
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    if (!dev && req.headers['x-forwarded-proto'] !== 'https') {
-      const { host } = parse(process.env.HOSTNAME);
-      res.writeHead(302, {
-        Location: `https://${host}${req.url}`
-      });
-      res.end();
-    } else {
-      handle(req, res, parsedUrl);
-    }
-  }).listen(port, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.use((req, res, next) => {
+      const hostname = process.env.HOSTNAME;
+      if (
+        req.headers['x-forwarded-proto'] === 'http' ||
+        req.hostname === process.env.HOSTNAME
+      ) {
+        res.redirect(301, `https://${hostname}${req.url}`);
+        return;
+      }
+
+      res.setHeader(
+        'strict-transport-security',
+        'max-age=31536000; includeSubDomains; preload'
+      );
+      next();
+    });
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(4242, error => {
+      if (error) throw error;
+      console.error('Listening on port 4242');
+    });
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
   });
-});
