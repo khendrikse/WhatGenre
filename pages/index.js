46c85424446f2c34e55489c4eb88d79868b59f02
khendrikse <30577427+khendrikse@users.noreply.github.com>
2022-08-28 10:53:04 +0200

    change favicon

--- pages/index.js
+++ pages/index.js
@@ -118,7 +118,12 @@ const Home = () => {
           rel='preload'
           as='font'
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='icon'
+          href='data:image/svg+xml,
+        <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+        <text y=%22.9em%22 font-size=%2290%22>🎧</text></svg>'
+        />
       </Head>
       <NavBar />
       <main>
